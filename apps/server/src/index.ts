5c1a38f0d103caff76cb4d3cbdce539e08812825
Wojciech Kluz <urban1996n@gmail.com>
2025-09-16 12:28:01 +0200

    Use IOC DI for the new services

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,43 +1,68 @@
-import express, { Request, Response, NextFunction } from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@as-integrations/express5';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { readFileSync } from 'fs';
+import {join} from 'path';
+import {Resolvers} from "apps/server/src/__generated__/graphql";
+import "reflect-metadata";
+import {setupServices} from "apps/server/config/services";
+import {container} from "tsyringe";
+import {
+    GetTrainsResolver
+} from "apps/server/src/modules/trains/infrastructure/presentation/resolvers/GetTrainsResolver";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
-const app = express()
+setupServices({container})
+    .then(() => {console.log('Services set up')})
+    .catch((err) => {console.error('Error setting up services', err)});
 
-app.use(morgan('dev'))
-app.use(cors())
-app.use(express.json())
+const trainsResolver = container.resolve<GetTrainsResolver>(GetTrainsResolver);
 
-// --- API ---
-app.get(
-    '/api/health',
-    (_req: Request, res: Response): void => {
-      res.status(200).send('ok')
-    }
-)
+// A map of functions which return data for the schema.
+const resolvers: Resolvers = {
+    Query: {
+        trains: () => trainsResolver.getTrains(),
+        train: (parent, args) => {
+            const id = args.id;
+            return trainsResolver.getTrains().then(trains => trains.find(train => train.id === id) || null);
+        }
+    },
+};
 
-const clientDist: string = path.resolve(__dirname, '../../client/dist')
+const schemaLocation = join('docs', 'schema.graphql');
 
-app.use(express.static(clientDist))
+const typeDefs = readFileSync(schemaLocation, 'utf8');
+const app = express();
+const httpServer = http.createServer(app);
 
-// --- SPA fallback ---
-app.get(
-    '*',
-    (req: Request, res: Response, next: NextFunction): void => {
-      if (req.path.startsWith('/api')) return next()
-      res.sendFile(path.join(clientDist, 'index.html'))
-    }
-)
+const log = () => {
+    console.log('log');
+}
 
-app.get('/api/health', (req: Request, response: Response) => {
-    response.send(true);
-})
+const logger = {
+    debug: log,
+    info: log,
+    warn: log,
+    error: log,
+}
 
-const PORT: number = Number(process.env.PORT ?? 3000)
+// Set up Apollo Server
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({httpServer})],
+    logger,
+});
 
-app.listen(PORT, () => {
-  console.log(`Server on http://localhost:${PORT}`)
-})
+await server.start();
+
+app.use(
+    cors(),
+    express.json(),
+    expressMiddleware(server),
+);
+
+httpServer.listen(4000);
+console.log(`🚀 Server ready at http://localhost:4000`);
