1e3fb530564b67d1c9eca444deda63db07fb789c
Wojciech Kluz <urban1996n@gmail.com>
2025-09-25 17:48:18 +0200

    Move common modules to shared directory

--- apps/server/src/app/interfaces/http/kernel/Loader.ts
+++
@@ -1,7 +0,0 @@
-import {DependencyContainer} from "tsyringe";
-
-export interface Loader
-{
-    onRegister(container: DependencyContainer): Promise<void>;
-    onBoot(container: DependencyContainer): Promise<void>;
-}
